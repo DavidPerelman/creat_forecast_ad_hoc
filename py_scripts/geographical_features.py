13b21d799f1b258e9ebcbc48ecb2f334617fb1a4
David Perelman <dperelman3@gmail.com>
2023-11-15 19:37:02 +0200

    geographical_features: up_load_gdb - muni_JTMT

--- py_scripts/geographical_features.py
+++ py_scripts/geographical_features.py
@@ -1,11 +1,9 @@
 from forecast import forecast
 from functions import make_point, up_load_gdb
 
-folder_path_save=r'C:\Users\dpere\Documents\JTMT\creat_forecast_ad_hoc\data'
-folder_path=r'{}\needed_files'.format(folder_path_save)
-gpd_name='subdistrict2008.gdb'
-
 forecast_point = make_point(forecast)
 
-subdistrict_il = up_load_gdb(r'{}\{}'.format(folder_path,gpd_name),'subdistrict2008_ITM')
-print(subdistrict_il)
+subdistrict_il=up_load_gdb( r'C:\Users\dpere\Documents\JTMT\creat_forecast_ad_hoc\data\needed_files\subdistrict2008.gdb','subdistrict2008_ITM')
+
+muni_JTMT=up_load_gdb( r'C:\Users\dpere\Documents\JTMT\creat_forecast_ad_hoc\data\needed_files\MUNI_border.gdb','muni_under_JTMT_ITM')
+
