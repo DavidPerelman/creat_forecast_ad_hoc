081796541527109204be4e588930340e511639a8
David Perelman <dperelman3@gmail.com>
2023-11-15 19:30:25 +0200

    geographical_features: up_load_gdb

--- py_scripts/functions.py
+++ py_scripts/functions.py
@@ -1,3 +1,12 @@
+import fiona
+import geopandas as gpd
+
+def up_load_gdb(path, layer_name):
+    path='{}'.format(path)
+    layer_list=fiona.listlayers(path)
+    gpd_layer=gpd.read_file(path, layer=layer_list.index(layer_name))
+    return gpd_layer
+
 def make_point(df):
     df_point=df.copy()
     df_point['centroid'] = df_point.representative_point()
--- py_scripts/geographical_features.py
+++ py_scripts/geographical_features.py
@@ -1,5 +1,11 @@
 from forecast import forecast
-from functions import make_point
+from functions import make_point, up_load_gdb
 
-forecast_point=make_point(forecast)
-print(forecast_point)
+folder_path_save=r'C:\Users\dpere\Documents\JTMT\creat_forecast_ad_hoc\data'
+folder_path=r'{}\needed_files'.format(folder_path_save)
+gpd_name='subdistrict2008.gdb'
+
+forecast_point = make_point(forecast)
+
+subdistrict_il = up_load_gdb(r'{}\{}'.format(folder_path,gpd_name),'subdistrict2008_ITM')
+print(subdistrict_il)
